d15bb2b5dd2a613d5e11fe82765eba6453b47541
eco-minio <eco@minio.com>
2025-02-14 10:58:07 -0800

    Change to Val's automation logic

--- format.sh
+++ format.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Format hdd 
-# List of values to map to physical devices
-#a=(c d e f g h i j k l m n);
-# List of values to map to drive LABEL 
-#b=(1 2 3 4 5 6 7 8 9 10 11 12 );
-#for i in ${!a[@]}; do mkfs.xfs -f -L drive${b[i]} /dev/sd${a[i]};
-#done
-
-
-# Format nvme with fstab output
-# List of values to map to physical devices
-# a=(0 1 2 3 4 5 6 7 8 9 10 11 13 14 15 16);
-# List of values to map to drive LABEL
-# b=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 );
-
+# Generate list of devices to format. This script is ***DANGEROUS*** and should not be used without confirming no unexpected devices are in the list, e.g. /
 devices=($(lsblk -J -o NAME,SIZE,ROTA,TYPE,MODEL,MOUNTPOINTS | jq -r '.blockdevices[] | select(.type == "disk" and (.children | length == 0)) | .name'))
 
+# Create mount point paths and format drives
 for i in ${!devices[@]} 
 do 
     sudo mkdir /mnt/minio-data${i}
     sudo mkfs.xfs -f -L minio-data${i} /dev/${devices[i]}
+done
+
+# Output fstab entries
+for i in ${!devices[@]}
+do 
     echo "LABEL=minio-data${i} /mnt/minio-data${i}  xfs     defaults,noatime        0 0"
 done
